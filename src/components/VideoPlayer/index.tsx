32133d34e6c481dbfd97d4f78f9f755b899158f1
e.bondarev <e.bondarev@inventos.ru>
2021-06-07 15:44:16 +0300

    Move to html5 player, in progress

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export interface IVideoPlayerProps extends HTMLVideoElement { }
 
-export const VideoPlayer: React.FC<IVideoPlayerProps> = React.memo((props) => {
+export const VideoPlayer: React.FC<IVideoPlayerProps> = (props) => {
   console.log('%c props ', 'background: #222; color: #bada55', props);
   const { srcObject } = props;
 
@@ -21,4 +21,4 @@ export const VideoPlayer: React.FC<IVideoPlayerProps> = React.memo((props) => {
   return (
     <video ref={ videoNode } style={{ border: '2px solid red' }}></video>
   );
-});
+};
