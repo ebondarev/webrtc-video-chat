b51478d4f879b070762e94110f97db2ddc794999
e.bondarev <e.bondarev@inventos.ru>
2021-06-07 14:35:20 +0300

    Move to html5 player, in progress

--- src/pages/RootChatPage/index.tsx
+++ src/pages/RootChatPage/index.tsx
@@ -23,6 +23,13 @@ export const RootChatPage: React.FC<IRootChatPage> = ({ peerId, peerJS }) => {
 
     peerJS.on('call', (call) => {
       console.log('%c oncall ', 'background: #222; color: #bada55', call);
+      call.on('stream', (stream: MediaStream) => {
+        console.log('%c stream ', 'background: #222; color: #bada55', stream);
+        const video = document.createElement('video') as HTMLVideoElement;
+        document.body.appendChild(video);
+        video.srcObject = stream;
+        video.style.border = '2px solid red';
+      });
     });
   }, [ peerJS ]);
 
