c07f484f5f4f2479cbad5e222a64f2c7e8c9a47b
e.bondarev <e.bondarev@inventos.ru>
2021-06-08 15:05:03 +0300

    Handle connections

--- src/pages/ClientChatPage/index.tsx
+++ src/pages/ClientChatPage/index.tsx
@@ -24,10 +24,13 @@ export const ClientChatPage: React.FC<IClientChatPageProps> = ({ peerId, idToCon
 
   React.useEffect(function listenRemoteData() {
     peerJS.on('connection', (connection: any) => {
-      console.log('%c client connection ', 'background: #222; color: #bada55', new Date().toUTCString());
-      connection.on('data', (data: any) => {
-        console.log('%c client. data ', 'background: #222; color: #bada55', data);
-      });
+      console.log('%c client connection ', 'background: #222; color: #bada55', Date.now(), connection);
+      connection.open('open', () => {
+        console.log('%c open ', 'background: #222; color: #bada55');
+        connection.on('data', (data: any) => {
+          console.log('%c client. data ', 'background: #222; color: #bada55', data);
+        });
+      })
     })
   }, [ peerJS ]);
 
