7746419b02633248481f0af8a0ff6f358c24d45c
e.bondarev <e.bondarev@inventos.ru>
2021-06-24 12:58:59 +0300

    .

--- src/pages/ClientChatPage/index.tsx
+++ src/pages/ClientChatPage/index.tsx
@@ -12,13 +12,27 @@ export const ClientChatPage: React.FC< IClientChatPageProps > = () => {
   const localStream = useLocalMediaStream();
 
   /* Клиент инициирует соединение с рутом, передаёт руту свой стрим и ожидает стрим от него */
-  const remoteRootMediaConnect = useExchangeMediaStreams(peerJS, rootPeerId, localStream)[0];
+  const remoteRootMediaConnect = useExchangeMediaStreams(peerJS, rootPeerId, localStream);
 
   const remotePeerIds = useRemotePeerData(peerJS);
   console.log('%c [полученные от root ids других клиентов]', 'background: #222; color: #bada55', remotePeerIds);
   // TODO: exchange data with other clients
-  const remotePeerMediaConnects = useExchangeMediaStreams(peerJS, remotePeerIds?.payload || [], localStream);
-  console.log('%c [remotePeerMediaConnects] ', 'background: #000; color: #fff;', remotePeerMediaConnects);
+  // const remotePeerMediaConnects = useExchangeMediaStreams(peerJS, remotePeerIds?.payload || [], localStream);
+  // console.log('%c [remotePeerMediaConnects] ', 'background: #000; color: #fff;', remotePeerMediaConnects);
+  React.useEffect(function exchangeStreams() {
+    if (remotePeerIds === undefined) {
+      return;
+    }
+    if (localStream === undefined) {
+      return;
+    }
+    remotePeerIds.payload.forEach((id) => {
+      const call = peerJS.call(id, localStream);
+      call.on('stream', (stream: MediaStream) => {
+        console.log('%c [stream, call] ', 'background: #222; color: #bada55', stream, call);
+      });
+    });
+  }, [ remotePeerIds ]);
 
   return (
     <div className="page">
