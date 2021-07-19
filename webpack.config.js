82db3d3544dbc8024a0d257f400e50b42e728a1b
e.bondarev <e.bondarev@inventos.ru>
2021-07-19 18:37:57 +0300

    Mobile version, in progress

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ const config = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "index.html",
+      meta: {
+        viewport: "width=device-width, initial-scale=1",
+      },
     }),
 
     // Add your plugins here
