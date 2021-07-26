a0fe3cbb9b882b129dc7553f601d5fff0405f082
e.bondarev <e.bondarev@inventos.ru>
2021-07-26 11:03:12 +0300

    IceServers and change http to https

--- src/index.ts
+++ src/index.ts
@@ -77,7 +77,33 @@ window.addEventListener('DOMContentLoaded', () => {
     avatar: 'https://cdn.iconscout.com/icon/free/png-256/avatar-366-456318.png',
   };
 
-  const peer  = new Peer({ debug: 1 });
+  const peer  = new Peer({
+    'iceServers': [
+      { url: 'stun:stun01.sipphone.com' },
+      { url: 'stun:stun.ekiga.net' },
+      { url: 'stun:stun.fwdnet.net' },
+      { url: 'stun:stun.ideasip.com' },
+      { url: 'stun:stun.iptel.org' },
+      { url: 'stun:stun.rixtelecom.se' },
+      { url: 'stun:stun.schlund.de' },
+      { url: 'stun:stun.l.google.com:19302' },
+      { url: 'stun:stun1.l.google.com:19302' },
+      { url: 'stun:stun2.l.google.com:19302' },
+      { url: 'stun:stun3.l.google.com:19302' },
+      { url: 'stun:stun4.l.google.com:19302' },
+      { url: 'stun:stunserver.org' },
+      { url: 'stun:stun.softjoys.com' },
+      { url: 'stun:stun.voiparound.com' },
+      { url: 'stun:stun.voipbuster.com' },
+      { url: 'stun:stun.voipstunt.com' },
+      { url: 'stun:stun.voxgratia.org' },
+      { url: 'stun:stun.xten.com' },
+      { url: 'turn:numb.viagenie.ca', credential: 'muazkh', username: 'webrtc@live.com' },
+      { url: 'turn:192.158.29.39:3478?transport=udp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username: '28224511:1379330808' },
+      { url: 'turn:192.158.29.39:3478?transport=tcp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username: '28224511:1379330808' },
+    ],
+    debug: 1,
+  });
   peer.on('open', function fetchPeerId(id: string) {
     (document.querySelector('.peer-id') as HTMLElement).innerText = id;
   });
@@ -125,7 +151,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const mainVideoElement = renderMainVideo(
       document.querySelector('.main-video') as HTMLElement,
       {
-        src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+        src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
         className: 'main-video__player',
       }
     );
