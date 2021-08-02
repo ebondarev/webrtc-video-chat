de3de132251de1f1b195a8249cae2e875d72f380
e.bondarev <e.bondarev@inventos.ru>
2021-08-02 17:48:59 +0300

    Revert "Add Video.js"
    This reverts commit 08d6a1161df7e93fba0e299233e86e68ad8addbb.

--- src/components/MainVideo/index.tsx
+++ src/components/MainVideo/index.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import videojs from 'video.js';
-import "video.js/dist/video-js.css";
 import s from './style.module.css';
 
 export const MainVideo = React.forwardRef<HTMLVideoElement, React.MediaHTMLAttributes<HTMLVideoElement>>((props, ref) => {
-	React.useEffect(() => {
-		console.log('[LOG]', 'ref', ref);
-	});
-	
 	return (
-		<div data-vjs-player>
-			<video {...props}
-				ref={ref}
-				className={s['main-video__player']}
-			></video>
-		</div>
+		<video {...props}
+			ref={ref}
+			className={s['main-video__player']}
+		></video>
 	)
 });
