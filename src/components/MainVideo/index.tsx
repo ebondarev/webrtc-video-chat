08d6a1161df7e93fba0e299233e86e68ad8addbb
e.bondarev <e.bondarev@inventos.ru>
2021-08-02 16:21:22 +0300

    Add Video.js

--- src/components/MainVideo/index.tsx
+++ src/components/MainVideo/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import videojs from 'video.js';
+import "video.js/dist/video-js.css";
 import s from './style.module.css';
 
 export const MainVideo = React.forwardRef<HTMLVideoElement, React.MediaHTMLAttributes<HTMLVideoElement>>((props, ref) => {
+	React.useEffect(() => {
+		console.log('[LOG]', 'ref', ref);
+	});
+	
 	return (
-		<video {...props}
-			ref={ref}
-			className={s['main-video__player']}
-		></video>
+		<div data-vjs-player>
+			<video {...props}
+				ref={ref}
+				className={s['main-video__player']}
+			></video>
+		</div>
 	)
 });
