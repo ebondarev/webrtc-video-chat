d0bce9e5f2e2625f487cd6a1435bf9168790546f
e.bondarev <e.bondarev@inventos.ru>
2021-08-05 14:10:50 +0300

    Add mute to local video

--- src/components/UsersVideo/index.tsx
+++ src/components/UsersVideo/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const UsersVideo: React.FC<Props> = ({streams}) => {
 	return (
 		<div className={s['users-video']}>
-			{streams.map((stream) => <VideoFromMediaStream key={stream.id} stream={stream} />)}
+			{streams.map((stream, index) => <VideoFromMediaStream key={stream.id} stream={stream} muted={index === 0} />)}
 		</div>
 	);
 };
--- src/components/VideoFromMediaStream/index.tsx
+++ src/components/VideoFromMediaStream/index.tsx
@@ -3,19 +3,20 @@ import s from './style.module.css';
 
 interface Props {
 	stream: MediaStream;
+	muted: boolean;
 }
 
-export const VideoFromMediaStream: React.FC<Props> = ({stream}) => {
+export const VideoFromMediaStream: React.FC<Props> = ({ stream, muted }) => {
 	const videoRef = React.useRef<HTMLVideoElement>(null);
 
 	React.useEffect(() => {
 		if (videoRef.current === null) return;
 		const video = videoRef.current;
 		video.srcObject = stream;
-		const startPlayback = () => video?.play();	
+		const startPlayback = () => video?.play();
 		video.addEventListener('loadedmetadata', startPlayback);
 		return () => video?.removeEventListener('loadedmetadata', startPlayback);
 	}, [stream]);
 
-	return (<video className={s['users-video__video']} ref={videoRef}></video>);
+	return (<video className={s['users-video__video']} ref={videoRef} muted={muted}></video>);
 };
